b317ab311754f08fd2799908ff080fb00fac2213
pome <pome1618@gmail.com>
2015-02-26 21:20:30 +0900

    add 002,003

---
+++ j1/002/answer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int a=45;
+	int b=1;
+	printf("a+b=%d\na-b=%d\n",a+b,a-b);
+	return 0;
+}
+
---
+++ j1/003/answer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a=35;
+	int b=88;
+	int c=154;
+	printf("a+b+c=%d\n",a+b+c);
+	printf("a-b-c=%d\n",a-b-c);
+	return 0;
+}
+
